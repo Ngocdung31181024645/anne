9e549a538622a7ee8ae781be997dd3f5e45bb2f7
zovt <zovt@posteo.de>
2017-12-30 15:53:43 -0500

    Basic library functionality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,40 @@
+extern crate env_logger;
+#[macro_use]
+extern crate log;
+
 mod library;
 mod filetypes;
 
-fn main() {}
+use std::env;
+use std::fs;
+use std::path::Path;
+use std::process;
+
+fn main() {
+	env_logger::init().expect("Failed to initialize logging");
+
+	let mut lib_path;
+	if let Some(home_dir) = env::home_dir() {
+		lib_path = home_dir;
+	} else {
+		error!("Could not get home directory");
+		process::exit(1);
+	};
+	lib_path.push(".local");
+	lib_path.push("share");
+	lib_path.push("alexandria");
+
+
+	if !lib_path.is_dir() {
+		info!("Creating library directory at: {}", lib_path.display());
+		if let Err(e) = std::fs::create_dir(&lib_path) {
+			error!("Failed to create library directory: {}", e);
+			process::exit(1);
+		};
+	}
+
+	let files = fs::read_dir(&lib_path).unwrap();
+	for file in files {
+		println!("Name: {}", file.unwrap().file_name().to_str().unwrap());
+	}
+}
